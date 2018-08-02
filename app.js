1dd3174d56975b96abddfcbe97cfb298a1718891
blakereeves19 <blake.reeves19@gmail.com>
2018-08-01 21:23:52 -0500

    Added the finishing touches to the JavaScript lab

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 let friends = ['Kyle', 'Will', 'Jacob', 'Bryant', 'Jp'];
 for(let i = 0; i < friends.length; i++) {
-    console.log(friends[i]);
-    for(let j = 99; j > 1; j--) {
-        console.log(j + ' lines of code in the file,' + ' ' + j + ' lines of code;' + friends[i] + 'strikes one out, clears it all out, lines of code in the file');
+    console.log(friends[i] + ':');
+    for(let j = 99; j > 2; j--) {
+        console.log(j + ' lines of code in the file,' + ' ' + j + ' lines of code;' + ' ' + friends[i] + ' ' + 'strikes one out, clears it all out,' + ' ' + [j - 1] + ' ' + 'lines of code in the file');
+    }
+    if (j = 2) {
+        console.log(j + ' lines of code in the file,' + ' ' + j + ' lines of code;' + ' ' + friends[i] + ' ' + 'strikes one out, clears it all out,' + ' ' + [1] + ' ' + 'line of code in the file');
+    }
+    if (j = 1) {
+        console.log(j + ' line of code in the file,' + ' ' + j + ' line of code;' + ' ' + friends[i] + ' ' + 'strikes one out, clears it all out, no more lines of code in the file');
     }
 }
